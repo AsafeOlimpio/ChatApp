752ff203983298341e6d0420d2ef84295ec6fe6a
LAPTOP-DRMMMS61\asafe <asafeolimpio@gmail.com>
2018-06-07 13:58:53 +0200

    Add new message comprovation

--- app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 public class NewMessageActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,22 @@ public class NewMessageActivity extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        username = findViewById(R.id.username_input);
+        message = findViewById(R.id.message_input);
     }
 
     public void enviar(View view) {
+        String usernameinput = username.getText().toString();
+        String messageinput = message.getText().toString();
+
+        if (TextUtils.isEmpty(usernameinput)){
+
+            username.setError(getString(R.string.empty_username));
+        }
+
+        else if (TextUtils.isEmpty(messageinput)){
+            message.setError(getString(R.string.empty_message));
+        }
     }
 }
