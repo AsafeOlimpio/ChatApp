02f9340cbe6810a16d3d0a8b373097727ac6a3e4
LAPTOP-DRMMMS61\asafe <asafeolimpio@gmail.com>
2018-06-07 12:56:39 +0200

    New Message Activity

--- app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
@@ -3,7 +3,10 @@ package com.android.teaching.chatapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -17,9 +20,21 @@ public class ChatActivity extends AppCompatActivity {
     private ListView listView;
 
     @Override
+    public boolean onCreatePanelMenu(int featureId, Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_chat_menu, menu);
+        return super.onCreatePanelMenu(featureId, menu);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.chat_activity_toolbar);
+        setSupportActionBar(myToolbar);
+        //No necesita parametros
+        getSupportActionBar();
 
         //myAdapter = new MyAdapter();
 
